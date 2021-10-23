6314ba6c435efb46e9e657c0a493073f1733a8b2
Sibian DG <sibiandegussem@outlook.com>
2021-10-23 09:22:06 +0200

    changes some installations & added option -y

--- install_software.ps1
+++ install_software.ps1
@@ -16,58 +16,47 @@ $Desktop = $Desktop.ToUpper()
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 #Installations:
-choco install googlechrome
-choco install firefox
-choco install vlc
-choco install git.install
-choco install python
-choco install winrar
-choco install malwarebytes
-choco install nodejs.install
-choco install ccleaner
-choco install microsoft-teams.install
-choco install teamviewer
-choco install filezilla
-choco install vscode.install
-choco install atom
-choco install virtualbox
-choco install microsoft-windows-terminal
-#choco install spotify
-choco install tor-browser
-choco install rufus
-#version
-choco install jdk11
-#choco install anaconda3
-#choco install discord.install
-#choco install jetbrainstoolbox
-#choco install javaruntime
-#choco install advanced-ip-scanner
-#choco install gitkraken
-#choco install droidcamclient
-#choco install garmin-express
-#choco install laragon.install
-#choco install protonvpn
-#choco install recuva
-#choco install speedtest
-#choco install signal
+choco install -y googlechrome
+choco install -y firefox
+choco install -y vlc
+choco install -y git.install
+choco install -y python
+choco install -y winrar
+choco install -y malwarebytes
+choco install -y nodejs.install
+choco install -y ccleaner
+choco install -y microsoft-teams.install
+choco install -y teamviewer
+choco install -y filezilla
+choco install -y vscode.install
+choco install -y atom
+choco install -y virtualbox
+choco install -y microsoft-windows-terminal
+choco install -y tor-browser
+choco install -y rufus
+choco install -y oraclejdk
+choco install -y anaconda3
+choco install -y discord.install
+choco install -y jetbrainstoolbox
+choco install -y javaruntime
+choco install -y advanced-ip-scanner
+choco install -y gitkraken
+choco install -y droidcamclient
+choco install -y garmin-express
+choco install -y laragon.install
+choco install -y protonvpn
+choco install -y recuva
+choco install -y speedtest
+choco install -y signal
 
 if ($Desktop.ToUpper() -eq "Y") {
-    choco install evoluentmousemanager
-    choco install minecraft
-	choco install icue
+    choco install -y evoluentmousemanager
+    choco install -y minecraft
+	choco install -y icue
 }
 
 
 #nog meer doen?
 #choco install wsl-ubuntu-1804
+#choco install spotify #not in Windows11
 
-#choco install scenebuilder9
-
-#choco install protonvpn
-
-#choco install mysql
-
-#todo: kaspersky
-#todo: Garmin
-#SQL Server
-#MYSQL
